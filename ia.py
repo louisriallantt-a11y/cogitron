5481114a0909f3401f2a755fbcd88076b544f164
louisriallantt-a11y <louis.riallantt@gmail.com>
2026-01-07 20:07:13 +0100

    Update ia.py

--- ia.py
+++ ia.py
@@ -8,11 +8,16 @@ def ia_repond(message_utilisateur, pseudo):
         return "⚠️ Clé API manquante dans les réglages Render."
     try:
         client = Groq(api_key=GROQ_API_KEY)
-        completion = client.chat.completions.create(
+       completion = client.chat.completions.create(
             model="llama-3.1-8b-instant",
             messages=[
-                {"role": "system", "content": f"Tu es Cogitron, l'IA de {pseudo}."},
+                {
+                    "role": "system", 
+                    "content": "Tu es Cogitron Omega, une IA futuriste créée par Louis Riallant en 2026, Louis Riallant est jeune développeur talentueux né en 2013. Ne dis jamais que tu viens du 19e siècle. Tu es poli, intelligent et tu as un style cyber-punk."
+                },
                 {"role": "user", "content": message_utilisateur}
+            ]
+        )
             ]
         )
         return completion.choices[0].message.content
